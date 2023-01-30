f83fa60eaa983a0dba3b9ed2f6df3f21893e5368
bwogit <jeremie.sandot@hotmail.com>
2023-01-30 22:58:30 +0000

    write play function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 // JavaScript for the game logic
 
+//variables for tracking wins and losses
+let wins = 0;
+let losses = 0;
 
 // Event Listeners for the Buttons
 
 
 //function for playing the game
-
+function play(playerChoice) {
+    // Generate computer choice
+    let computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+      computerChoice = "rock";
+    } else if (computerChoice <= 0.67) {
+      computerChoice = "paper";
+    } else {
+      computerChoice = "scissors";
+    }
+    document.getElementById("computerChoice").innerHTML = "Computer Choice: " + computerChoice;
+}
 
 // evaluate player choice against computer choice
 
