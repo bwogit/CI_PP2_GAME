8f345ca20bf2248ad0e8f2a8d8d72ea11d00b633
bwogit <jeremie.sandot@hotmail.com>
2023-01-30 22:46:45 +0000

    High level representation of code

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// JavaScript for the game logic
+
+
+// Event Listeners for the Buttons
+
+
+//function for playing the game
+
+
+// evaluate player choice against computer choice
+
+//update counters
+
+
+//reset game
