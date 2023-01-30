d0ba7088c016571bc6a21592a7fd7aa240b85d31
bwogit <jeremie.sandot@hotmail.com>
2023-01-30 23:19:43 +0000

    evaluate player chhoices vs computer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,43 @@ function play(playerChoice) {
     document.getElementById("computerChoice").innerHTML = "Computer Choice: " + computerChoice;
 }
 
-// evaluate player choice against computer choice
+// evaluate player choice against computer choice there are 3 scenarios possible
+if (playerChoice === computerChoice) {
+    // 1 Tie
+    document.getElementById("result").innerHTML = `You both chose ${playerChoice} It's a Tie!`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    // 2 Player wins
+    wins++;
+    document.getElementById("result").innerHTML = playerChoice+ " beats " + computerChoice + " : You Win! ";
+  } else {
+    // 3 Player loses
+    losses++;
+    document.getElementById("result").innerHTML =computerChoice+" beats "+ playerChoice + " : Loooser!";
+  }
 
 //update counters
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
 
+//game ends after computer wins 3 times
+if (losses === 3) {
+    // endGame();
+    resetGame();
+    alert("Game over!");
+}
 
 //reset game
+function resetGame() {
+    wins = 0;
+    losses = 0;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("playerChoice").innerHTML = "";
+    document.getElementById("computerChoice").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
+    
+  }
