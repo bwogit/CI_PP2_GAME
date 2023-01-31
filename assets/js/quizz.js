396056306ac1c904044633a6af44eac853d9b02c
bwogit <jeremie.sandot@hotmail.com>
2023-01-31 21:04:02 +0000

    quizz.js\construct general structure of the code

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -1,5 +1,16 @@
-// select quizz HTML document and save them in variables
+// select quizz element from HTML document and save them in variables
 let quizzArea = document.getElementById("quizz");
 let resultArea = document.getElementById("results");
 let submitButton = document.getElementById("submit");
 
+function runQuizz() {
+
+}
+
+function checkResults() {
+
+}
+
+runQuizz();
+
+// dispaly results when click submit
