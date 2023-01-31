a752698b977b02aff45f3fca7e47a5c79d926140
bwogit <jeremie.sandot@hotmail.com>
2023-01-31 17:36:49 +0000

    quizz.js| declare variables

---
+++ assets/js/quizz.js
@@ -0,0 +1,5 @@
+// select quizz HTML document and save them in variables
+let quizzArea = document.getElementById("quizz");
+let resultArea = document.getElementById("results");
+let submitButton = document.getElementById("submit");
+
