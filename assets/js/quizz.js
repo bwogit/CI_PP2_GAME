a95ec3324b9e13ca674a810f7004622b9b1768b5
bwogit <jeremie.sandot@hotmail.com>
2023-02-01 15:25:17 +0000

    Several modification required to style .css to make it ersponsive

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -124,36 +124,36 @@ const rpsQuestions = [
         },
         correctAnswer: "d" 
     },
-    {
-        question: "What is it called in french?",
-        choices:{
-            a:"un, deux, trois",
-            b:"s'il te plait",
-            c:"pret a porter",
-            d:"chi fou mi"
-        },
-        correctAnswer: "d" 
-    },
-    {
-        question: "What is it called in french?",
-        choices:{
-            a:"un, deux, trois",
-            b:"s'il te plait",
-            c:"pret a porter",
-            d:"chi fou mi"
-        },
-        correctAnswer: "d" 
-    },
-    {
-        question: "in which dynasty was the game created?",
-        choices:{
-            a:"han",
-            b:"ming",
-            c:"satay",
-            d:"feng shui"
-        },
-        correctAnswer: "a" 
-    },
+    // {
+    //     question: "What is it called in french?",
+    //     choices:{
+    //         a:"un, deux, trois",
+    //         b:"s'il te plait",
+    //         c:"pret a porter",
+    //         d:"chi fou mi"
+    //     },
+    //     correctAnswer: "d" 
+    // },
+    // {
+    //     question: "What is it called in french?",
+    //     choices:{
+    //         a:"un, deux, trois",
+    //         b:"s'il te plait",
+    //         c:"pret a porter",
+    //         d:"chi fou mi"
+    //     },
+    //     correctAnswer: "d" 
+    // },
+    // {
+    //     question: "in which dynasty was the game created?",
+    //     choices:{
+    //         a:"han",
+    //         b:"ming",
+    //         c:"satay",
+    //         d:"feng shui"
+    //     },
+    //     correctAnswer: "a" 
+    // },
 
 ];
 shuffleQuestions();
