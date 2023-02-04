4a1f290d7db2dac4c81df0ccf43030544bae0e69
bwogit <jeremie.sandot@hotmail.com>
2023-02-04 09:37:50 +0000

    form.js\susccess email sent

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,10 @@
 
+
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init('rUyj3ASnTOPeeYh8M');
+})();
+
 // Get the form element from the HTML using its ID
 var form = document.getElementById("form");
 
@@ -8,7 +14,7 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // Use the emailjs.sendForm method to send the form data as an email
-  emailjs.sendForm("service_0386yf2","template_79nexyb", this)
+  emailjs.sendForm("service_form","template_form", this)
     // If the email was successfully sent, log a success message to the console
     .then(function (response) {
       console.log('SUCCESS!', response.status, response.text);
