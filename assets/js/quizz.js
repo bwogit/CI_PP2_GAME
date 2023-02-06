799db8345f82f2bd2e01ddabe87048727fdf9d65
bwogit <jeremie.sandot@hotmail.com>
2023-02-06 16:35:42 +0000

    quizz.js\moving the rpsQuestion variable to the top of the code

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -1,96 +1,3 @@
-/**
- * shuffleQuestions randomly shuffles the questions shuffledQuestions
- */
-//declaring variable shuffledQuestions
-let shuffledQuestions;
-
-function shuffleQuestions(){
-    shuffledQuestions = rpsQuestions.sort(() => Math.random() - 0.5);
-  }
-  //display quizz on page
-function runQuizz() {
-    // store the HTML output in variable htmlOutput
-    let htmlOutput =[];
-    
-    //loop through each index (question) and the question number in the array. 
-    
-    // rpsQuestions.forEach (
-    //     (index,questionNumber)=> {
-        //loop through the first 4 ietms in the array
-    for (let i = 0; i < 4; i++) {
-        let index = rpsQuestions[i];
-        let questionNumber = i;
-
-    //storing all the answers into an array called choices
-    let choices =[];
-    
-    // for each answer in the array build a button
-    for(letter in index.choices){
-    
-        // ...add an HTML radio button
-        choices.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${index.choices[letter]}
-          </label>`
-        );
-      }    
-    // Join  questions and  answers to the output
-
-    htmlOutput.push (
-    `<div class="question"> ${index.question} </div>
-    <div class="answers"> ${choices.join('')} </div>`
-    );
-    }
-// );
-quizzArea.innerHTML = htmlOutput.join('');
-}
-
-
-function checkResults() {
-
-// harvest all possible choices from the quizz
-let choicesContainers = quizzArea.querySelectorAll(".answers");
-
-//count corect answers
-let ansCorrect = 0;
-
-//loop trhough each question and check right answrer
-
-// rpsQuestions.forEach( (index, questionNumber) => {
-    for (let i = 0; i < 4; i++) {
-        let index = rpsQuestions[i];
-        let questionNumber = i; 
-// find selected answer
-const choicesContainer = choicesContainers[questionNumber];
-const selector = `input[name=question${questionNumber}]:checked`;
-// preparing for incomplete user input
-const userAnswer = (choicesContainer.querySelector(selector) || {}).value;
-
-//evaluate the answers
-//is the answer is correct
-if(userAnswer === index.correctAnswer){
-    // increment correct answers
-    ansCorrect++;
-    //display green font
-    choicesContainers[questionNumber].style.color ="green"
-}
-else {choicesContainers[questionNumber].style.color ="red"} 
-    
-}
-// )
-// Display totalnumber of  correct answers
-// resultArea.innerHTML = `${ansCorrect} out of ${rpsQuestions.length}`;
-resultArea.innerHTML = `${ansCorrect} out of 4`; 
-};
-
-
-// select quizz element from HTML document and save them in variables
-let quizzArea = document.getElementById("quizz");
-let resultArea = document.getElementById("results");
-let submitButton = document.getElementById("submit");
-
 //create the list of questions
 
 const rpsQuestions = [
@@ -225,6 +132,101 @@ const rpsQuestions = [
             correctAnswer: "c" 
     }
 ];
+/**
+ * shuffleQuestions randomly shuffles the questions shuffledQuestions
+ */
+//declaring variable shuffledQuestions
+let shuffledQuestions;
+
+function shuffleQuestions(){
+    shuffledQuestions = rpsQuestions.sort(() => Math.random() - 0.5);
+  }
+  //display quizz on page
+function runQuizz() {
+    // store the HTML output in variable htmlOutput
+    let htmlOutput =[];
+    
+    //loop through each index (question) and the question number in the array. 
+    
+    // rpsQuestions.forEach (
+    //     (index,questionNumber)=> {
+        
+    //loop through the first 4 ietms in the array
+    for (let i = 0; i < 4; i++) {
+        let index = rpsQuestions[i];
+        let questionNumber = i;
+
+    //storing all the answers into an array called choices
+    let choices =[];
+    
+    // for each answer in the array build a button
+    for(letter in index.choices){
+    
+        // ...add an HTML radio button
+        choices.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${index.choices[letter]}
+          </label>`
+        );
+      }    
+    // Join  questions and  answers to the output
+
+    htmlOutput.push (
+    `<div class="question"> ${index.question} </div>
+    <div class="answers"> ${choices.join('')} </div>`
+    );
+    }
+// );
+quizzArea.innerHTML = htmlOutput.join('');
+}
+
+
+function checkResults() {
+
+// harvest all possible choices from the quizz
+let choicesContainers = quizzArea.querySelectorAll(".answers");
+
+//count corect answers
+let ansCorrect = 0;
+
+//loop trhough each question and check right answrer
+
+// rpsQuestions.forEach( (index, questionNumber) => {
+    for (let i = 0; i < 4; i++) {
+        let index = rpsQuestions[i];
+        let questionNumber = i; 
+// find selected answer
+const choicesContainer = choicesContainers[questionNumber];
+const selector = `input[name=question${questionNumber}]:checked`;
+// preparing for incomplete user input
+const userAnswer = (choicesContainer.querySelector(selector) || {}).value;
+
+//evaluate the answers
+//is the answer is correct
+if(userAnswer === index.correctAnswer){
+    // increment correct answers
+    ansCorrect++;
+    //display green font
+    choicesContainers[questionNumber].style.color ="green"
+}
+else {choicesContainers[questionNumber].style.color ="red"} 
+    
+}
+// )
+// Display totalnumber of  correct answers
+// resultArea.innerHTML = `${ansCorrect} out of ${rpsQuestions.length}`;
+resultArea.innerHTML = `${ansCorrect} out of 4`; 
+};
+
+
+// select quizz element from HTML document and save them in variables
+let quizzArea = document.getElementById("quizz");
+let resultArea = document.getElementById("results");
+let submitButton = document.getElementById("submit");
+
+
 shuffleQuestions();
 runQuizz();
 
